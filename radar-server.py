b3ab914ac35f18bac0a3125fbe303d1ffc0cd032
TJ Turnage <t.j.turnage@gmail.com>
2024-04-15 12:56:59 -0400

    - changed host address

--- radar-server.py
+++ radar-server.py
@@ -340,4 +340,4 @@ def get_duration(duration):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050, threaded=True)
+    app.run_server(debug=True, host="0.0.0.0", port=8050, threaded=True)
