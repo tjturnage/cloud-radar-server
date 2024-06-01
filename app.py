668aba48fb5ffc93bafa08e8c36dca2b811ec19f
TJ Turnage <t.j.turnage@gmail.com>
2024-06-01 15:31:35 -0400

    - more hodo argument fixes

--- app.py
+++ app.py
@@ -497,8 +497,8 @@ def launch_obs_script(n_clicks):
             except Exception as e:
                 print("Error running nexrad script: ", e)
             try:
-                print(f'hodo script: {radar}, {sa.new_radar}, {BASE_DIR}, {asos_one}, {asos_two}')
-                run_hodo_script([BASE_DIR, radar, sa.new_radar, asos_one, asos_two, sa.simulation_seconds_shift])
+                print(f'hodo script:  {BASE_DIR}, {radar}, {sa.new_radar}, {asos_one}, {asos_two}, {sa.simulation_seconds_shift}')
+                run_hodo_script([BASE_DIR, radar, sa.new_radar, asos_one, asos_two, str(sa.simulation_seconds_shift)])
                 print("Hodograph script completed ...")
             except Exception as e:
                 print("Error running hodo script: ", e)
