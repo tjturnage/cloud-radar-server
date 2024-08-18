38769773df9f9d457b18a3cdb1b92de34d6fa634
TJ Turnage <t.j.turnage@gmail.com>
2024-08-18 13:15:43 -0400

    - User Guide link at to opens the associated Google Doc in a separate tab - Feedback Form, GitHub, and Hodograph links also now open in separate tabs - update_hodo_page script now adds the following to hodographs.html -- a dropdown option for radar sites -- a slider to loop through images -- a favicon that indicates this html page is related to the cloud server

--- scripts/update_hodo_page.py
+++ scripts/update_hodo_page.py
@@ -8,7 +8,7 @@ import sys
 from datetime import datetime
 import pytz
 
-from config import HODOGRAPHS_DIR, HODOGRAPHS_PAGE
+#from config import HODOGRAPHS_DIR, HODOGRAPHS_PAGE
 
 #HODO_DIR = '/data/cloud-radar-server/assets/hodographs'
 #HODOGRAPHS_PAGE = '/data/cloud-radar-server/assets/hodographs.html'
@@ -16,8 +16,8 @@ dir_parts = Path.cwd().parts
 if 'C:\\' in dir_parts:
     HODOGRAPHS_DIR = 'C:/data/scripts/cloud-radar-server/assets/hodographs'
     HODOGRAPHS_PAGE = 'C:/data/scripts/cloud-radar-server/assets/hodographs.html'
-    link_base = "http://localhost:8050/assets"
-    cloud = False
+    #link_base = "http://localhost:8050/assets"
+    #cloud = False
 
 
 HEAD = """<!DOCTYPE html>
@@ -31,7 +31,8 @@ HEAD = """<!DOCTYPE html>
 HEAD_NOLIST = """<!DOCTYPE html>
 <html>
 <head>
-<title>Sim Hodos/title>
+<title>Hodographs</title>
+<link rel="icon" type="image/x-icon" href="./favicon.ico">
 </head>
 <body>"""
 
@@ -59,7 +60,8 @@ class UpdateHodoHTML():
             try:
                 self.clock_time = datetime.strptime(self.clock_str,"%Y-%m-%d %H:%M").replace(tzinfo=pytz.UTC).timestamp()
                 self.valid_hodo_list = self.make_valid_hodo_list()
-                self.update_hodo_page()
+                #self.update_hodo_page()
+                self.update_hodographs_loop_page()
             except ValueError as ve:
                 print(f'Could not decode current playback time in UpdateHodoHTML: {ve}')
                 self.clock_time = 'None'
@@ -70,9 +72,10 @@ class UpdateHodoHTML():
         Returns a list of valid hodographs based on the current playback time
         """
         valid_hodo_list = []
-        image_files = [f for f in os.listdir(HODOGRAPHS_DIR) if f.endswith('.png') or f.endswith('.jpg')]
-        
-        for filename in image_files:
+        self.image_files = [f for f in os.listdir(HODOGRAPHS_DIR) if f.endswith('.png') or f.endswith('.jpg')]
+        self.first_image_path = f'hodographs/{self.image_files[0]}'
+        print(self.image_files)
+        for filename in self.image_files:
             file_time = datetime.strptime(filename[-19:-4], '%Y%m%d_%H%M%S').replace(tzinfo=pytz.UTC).timestamp()
             if file_time < self.clock_time:
                 valid_hodo_list.append(filename)
@@ -105,6 +108,114 @@ class UpdateHodoHTML():
                         fout.write(line)
                 fout.write(TAIL)
 
+    def update_hodographs_loop_page(self) -> None:
+        """
+        Creates slider loop in hodographs.html page based on the current playback time
+        """
+
+        if len(self.valid_hodo_list) == 0:
+            self.initialize_hodo_page()
+
+        else:
+            station_codes = set(img[-24:-20] for img in self.image_files)
+        # HTML content with JavaScript to loop through images using a slider
+            html_content = f"""<!DOCTYPE html>
+            <html>
+            <head>
+            <title>Hodographs</title>
+            <style>
+            body {{ font-family: Arial, sans-serif; }}
+            img {{ max-width: 60%; height: auto; }}
+            .slider-container {{
+                width: 100%;
+                text-align: center;
+            }}
+            .slider-text-container {{
+                font-size: 2em;
+                font-style: strong;
+                width: 100%;
+                text-align: center;
+            }}
+            .slider {{
+                width: 80%;
+            }}
+            #image-container {{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+            }}
+            #station-container {{
+                display: none;
+                margin-left: 20px;
+                font-size: 1.5em;
+                font-weight: bold;
+            }}
+            </style>
+            <link rel="icon" type="image/x-icon" href="./favicon.ico">
+            </head>
+            <body>
+            <h1>Hodographs</h1>
+            <div class="slider-text-container">Playback time: {self.clock_str}</div>
+            <div id="controls">
+            <label for="station-dropdown">Select Station:</label>
+            <select id="station-dropdown">
+                <option value="all">All</option>
+                {"".join(f'<option value="{code}">{code}</option>' for code in station_codes)}
+            </select>
+            </div>
+            <div id="image-container">
+            <img id="hodograph-image" src="hodographs/{self.image_files[0]}" alt="Hodograph">
+            <div id="station-container"></div>
+            </div>
+            <br><br>
+            <div class="slider-text-container">Choose which half of the slider range to use based on preferred hodo type</div>
+            <div class="slider-container">
+            <input type="range" min="0" max="{len(self.image_files) - 1}" value="0" class="slider" id="image-slider">
+            </div>
+            <div class="slider-text-container">Ground Relative&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp | &nbsp&nbsp&nbsp&nbsp&nbsp&nbspStorm Relative</div>
+
+            <script>
+            var allImages = { [f'hodographs/{img}' for img in self.image_files] };
+            var images = allImages;
+            var slider = document.getElementById('image-slider');
+            var dropdown = document.getElementById('station-dropdown');
+
+            function updateSlider() {{
+                var selectedStation = dropdown.value;
+                if (selectedStation === 'all') {{
+                images = allImages;
+                }} else {{
+                images = allImages.filter(img => img.includes(selectedStation));
+                }}
+                slider.max = images.length - 1;
+                slider.value = 0;
+                document.getElementById('hodograph-image').src = images[0];
+                updateStationCode(images[0]);
+            }}
+
+            function updateStationCode(imagePath) {{
+                var stationCode = imagePath.slice(-24, -20);
+                document.getElementById('station-container').innerText = stationCode;
+            }}
+
+            slider.oninput = function() {{
+                var selectedImage = images[this.value];
+                document.getElementById('hodograph-image').src = selectedImage;
+                updateStationCode(selectedImage);
+            }};
+
+            dropdown.onchange = function() {{
+                updateSlider();
+            }};
+
+            updateSlider(); // Initialize the slider with the default selection
+            </script>
+            </body>
+            </html>"""
+            with open(HODOGRAPHS_PAGE, 'w', encoding='utf-8') as fout:
+                fout.write(html_content)
+
+
 if __name__ == "__main__":
-    #this_playback_time = '2024-06-01 23:15 UTC'
+    #UpdateHodoHTML('2024-09-01 23:15')
     UpdateHodoHTML(sys.argv[1])
