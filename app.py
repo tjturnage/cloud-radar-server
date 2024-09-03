c590e8f85495a839abe372c56c1e8ec83084e667
TJ Turnage <t.j.turnage@gmail.com>
2024-09-03 19:52:49 -0400

    - revert app env settings

--- app.py
+++ app.py
@@ -1277,14 +1277,13 @@ def update_day_dropdown(selected_year, selected_month):
 ################################################################################################
 
 if __name__ == '__main__':
-    app.run_server(host="0.0.0.0", port=8050, threaded=True, debug=True, use_reloader=False,
-                        dev_tools_hot_reload=False)
-    # if config.CLOUD:
-    #     app.run_server(host="0.0.0.0", port=8050, threaded=True, debug=True, use_reloader=False,
-    #                    dev_tools_hot_reload=False)
-    # else:
-    #     if config.PLATFORM == 'DARWIN':
-    #         app.run(host="0.0.0.0", port=8051, threaded=True, debug=True, use_reloader=False,
-    #             dev_tools_hot_reload=False)
-    #     else:
-    #         app.run(debug=True, port=8050, threaded=True, dev_tools_hot_reload=False)
+
+    if config.CLOUD:
+        app.run_server(host="0.0.0.0", port=8050, threaded=True, debug=True, use_reloader=False,
+                       dev_tools_hot_reload=False)
+    else:
+        if config.PLATFORM == 'DARWIN':
+            app.run(host="0.0.0.0", port=8051, threaded=True, debug=True, use_reloader=False,
+                dev_tools_hot_reload=False)
+        else:
+            app.run(debug=True, port=8050, threaded=True, dev_tools_hot_reload=False)
