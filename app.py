ab5a0fead7b77542422aac90310df50adc7198ab
lcarlaw <leecarlaw@gmail.com>
2025-03-07 13:54:56 -0600

    Removed initial call to write_event_times.py as it was failing within the append_radar_file_info() block due to radarinfo.json not being available at this point. Errors were being written to scripts.txt logfile.

--- app.py
+++ app.py
@@ -783,14 +783,6 @@ def run_with_cancel_button(cfg, sim_times, radar_info):
     This version of the script-launcher trying to work in cancel button
     """
     UpdateHodoHTML('None', cfg['HODOGRAPHS_DIR'], cfg['HODOGRAPHS_PAGE'])
-    # writes a black event_times.txt file to the assets directory
-    args = [str(sim_times['simulation_seconds_shift']), 'None', cfg['RADAR_DIR'],
-            cfg['EVENTS_HTML_PAGE'], cfg['EVENTS_TEXT_FILE']]
-    res = call_function(utils.exec_script, Path(cfg['EVENT_TIMES_SCRIPT_PATH']), args,
-                        cfg['SESSION_ID'])
-    if res['returncode'] in [signal.SIGTERM, -1*signal.SIGTERM]:
-        return
-
 
     # based on list of selected radars, create a dictionary of radar metadata
     try:
